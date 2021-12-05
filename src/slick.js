bbb246be0e19cf8ab450491a56a10514d15088b8
Oliver Huynh <oliver@jufist.com>
2021-12-06 02:25:28 +0700

    Init

---
+++ src/slick.js
@@ -0,0 +1,53 @@
+export const init = (config) => {
+  const json = config
+    .map((select) => (typeof select === 'string' ? { select } : select))
+    .map((select) => ({
+      ...{
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true
+      },
+      ...select
+    }))
+    .reduce((o, { select, ...i }) => ({ ...o, ...{ [select]: i } }), {});
+  (function ($, Drupal, drupalSettings) {
+    /* ELEMENT: SLICK */
+    Drupal.behaviors.haclaslick = {
+      doslick: function (options) {
+        this.not('.slick-initialized').slick(options);
+      },
+      attach: function attach(context) {
+        const t = this;
+        const select = json;
+
+        // My responsive slick
+        for (const sl of Object.keys(select)) {
+          for (const o of $(context).find(sl).once('sla').toArray()) {
+            const { desktop = false, ...selectobj } = select[sl];
+            $(window).on('toMobile', () => {
+              try {
+                if (desktop) {
+                  $(o).slick('unslick');
+                }
+              } catch (e) {}
+
+              t.doslick.bind($(o))(selectobj);
+            });
+            $(window).on('toWeb', () => {
+              try {
+                $(o).slick('unslick');
+              } catch (e) {}
+
+              if (desktop) {
+                t.doslick.bind($(o))({ ...selectobj, ...desktop });
+              }
+            });
+          }
+        }
+      }
+    };
+  })(jQuery, Drupal, drupalSettings);
+}
